94ed637790a5e078f675d4c455a32f6b9e1b9371
Hilmi Irfani <hilmiirfani04804@gmail.com>
2025-10-28 23:02:01 -0700

    Understanding JSX in ReactJS and ES6

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import UserCard from './UserCard';
+import { getUsers } from './Services';
 
 function Exercise() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getUsers();
+                setUsers(data);
+            } catch (error) {
+                console.error("[Component] Gagal menampilkan data:", error.message);
+            }
+        };
+        fetchData();
+    }, []);
+
+
     return (
         <>
-            <div className="min-h-screen bg-gray-100 p-6">
-                <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+            <div className='min-h-screen bg-gray-100 p-6'>
+                <h1 className='text-3xl font-bold text-center mb-6 text-blue-700'>
                     User Cards
                 </h1>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-                    <UserCard
-                        name="Hilmi"
-                        email="hilmi@example.com"
-                        street="Jl. Udowo"
-                        city="Semarang"
-                    />
-                    <UserCard
-                        name="Fauzi"
-                        email="fauzi@example.com"
-                        street="Jl. Bandungsari"
-                        city="Brebes"
-                    />
-                    <UserCard
-                        name="Haikal"
-                        email="hilmi@example.com"
-                        street="Jl. Cipajang"
-                        city="Brebes"
-                    />
+                    {users.map((user) => (<UserCard key={user.email}{...user} />))}
                 </div>
             </div>
         </>
-    );
+    )
 }
 
 export default Exercise;
